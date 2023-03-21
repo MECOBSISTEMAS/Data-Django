238b72fc4cb85fb3f7dc820a9938829cdb206a95
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2023-03-21 02:14:02 -0300

    usar o CORE_DIR ao inves do BASE_dIR

--- core/settings.py
+++ core/settings.py
@@ -105,7 +105,8 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': os.path.join(CORE_DIR, 'db.sqlite3'),
+            
         }
     }
 
