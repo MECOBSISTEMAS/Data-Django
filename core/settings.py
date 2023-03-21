c457efa36a8645d23f9d8c804d8eca1441b2d580
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2023-03-21 02:08:04 -0300

    ERRO: Colação de caracteres

--- core/settings.py
+++ core/settings.py
@@ -105,7 +105,10 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'NAME': BASE_DIR / 'db.sqlite3',
+                'OPTIONS': {
+            'charset': 'utf8mb4',
+            },
         }
     }
 db_from_env = dj_database_url.config(conn_max_age=600)
