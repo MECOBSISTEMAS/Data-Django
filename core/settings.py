1317b3459be78a6ede5a1f08a3ce6e237f8cb611
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2023-03-21 02:04:03 -0300

    forçar o carregamento do sqlite

--- core/settings.py
+++ core/settings.py
@@ -103,13 +103,13 @@ if os.environ.get('DB_ENGINE') and os.environ.get('DB_ENGINE') == "mysql":
     }
 else:
     DATABASES = {
-        """ 'default': {
+        'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': 'db.sqlite3',
-        } """
-        #configure o sqlite para ser executado caso não seja o MySQL
-        'default': {dj_database_url.config(conn_max_age=600)}
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
