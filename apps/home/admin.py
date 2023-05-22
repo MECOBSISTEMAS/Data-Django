7efbfc23ed86a0cfffc929ed1d346025077fea76
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2023-05-22 14:26:14 -0300

    Modelos cadastrados no Django Admin

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -4,5 +4,14 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.contrib import admin
-
+from apps.home.models import Credito, Debito, RepasseRetido, RepasseAprovado, Taxa, CadCliente
+from apps.home.existing_models import Pessoas
 # Register your models here.
+
+admin.site.register(Credito)
+admin.site.register(Debito)
+admin.site.register(RepasseRetido)
+admin.site.register(RepasseAprovado)
+admin.site.register(Taxa)
+admin.site.register(CadCliente)
+admin.site.register(Pessoas)
