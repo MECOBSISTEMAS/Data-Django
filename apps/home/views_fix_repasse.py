4f9f3e3a89fdc8d30ac48a72276ccbdece3e9022
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2023-07-11 17:18:28 -0300

    model.defer no repasses_retidos

--- apps/home/views_fix_repasse.py
+++ apps/home/views_fix_repasse.py
@@ -168,7 +168,7 @@ def pages(request):
                     context['taxas'] = float(Dado.objects.filter(dt_credito=data, banco=str(bancos).upper()).aggregate(taxas=Sum('taxas'))['taxas'] or 0)
                     repasses_geral = float(context['repasses_geral']['repasses'] or 0)
                     repasses_semanais_vendedores_totais = (sum([float(querie['total_repasses']) for querie in context['repasses_semanais']]))
-                    repasses_geral_descontado = repasses_geral - repasses_semanais_vendedores_totais
+                    repasses_geral_descontado =  repasses_semanais_vendedores_totais - repasses_geral
                     
                     context['repasses_geral_descontado'] = float("{:.2f}".format(repasses_geral_descontado))
                     
