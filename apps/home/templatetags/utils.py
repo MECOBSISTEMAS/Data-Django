06b23a8471f7ca6da2973f1e307dde4b7605cf83
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2023-08-08 17:15:29 -0300

    intcomma nas taxas

--- apps/home/templatetags/utils.py
+++ apps/home/templatetags/utils.py
@@ -1,3 +1,11 @@
 from django.template import Library
+from django.utils.formats import number_format
 
 register = Library()
+
+@register.filter(name='custom_number_format')
+def custom_number_format(value):
+    # Substitui pontos por vírgulas e vice-versa
+    value = value.replace(',', '|').replace('.', ',').replace('|', '.')
+
+    return value
