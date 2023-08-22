1710dc9a0e4772871b3104460d51e6834e26b142
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2023-08-22 15:22:43 -0300

    Botões de checagem de valores

--- apps/static/assets/js/utils.js
+++ apps/static/assets/js/utils.js
@@ -5,9 +5,7 @@ function getCookie(cookieName="") {
   .find(cookie => cookie.startsWith(cookieName + '='))
 
   if (cookie) {
-    //! retornar o valor do cookie sem o nome do cookie
     return cookie.split('=')[1]
   }
   return ""
 }
-
