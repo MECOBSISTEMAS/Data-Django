7f2b40553d923ab476cc490ddb2eb17cc9e29a5d
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2023-09-01 13:37:27 -0300

    alteração dos adicionamento dos vendedores

--- apps/home/components/registrar_contratos.py
+++ apps/home/components/registrar_contratos.py
@@ -12,17 +12,11 @@ from django.shortcuts import render
 from django.core.paginator import Paginator, Page, PageNotAnInteger, EmptyPage
 from django.utils import timezone
 from django.core import serializers
-import random
-import tempfile
-import os
-import json
-import openpyxl
-import asyncio
-import calendar
 
 from decimal import Decimal
 from openpyxl.utils import get_column_letter
 from requests import Request
+import requests
 
 from apps.home.existing_models import Contratos, ContratoParcelas, Pessoas, Eventos
 from apps.home.models import CadCliente, Debito, Credito, Taxa, RepasseRetido, Dado, RepasseAprovado, ParcelaTaxa
@@ -36,22 +30,41 @@ class RegistrarContratosView(UnicornView):
     contrato_existe:bool = False
     #campos para registros
     id_pessoa:int = None
-    
-    listas_pessoas:list[QuerySetType[Pessoas]] = []
+    peso:float = 0
+    id_contrato:int = None
+    #listas de objetos
+    listas_pessoas:list = []
     pessoa:Pessoas = None
+    contrato:Contratos = Contratos.objects.none()
+    
+    def checar_contrato(self):
+        sql_query = f"SELECT * FROM contratos WHERE contratos.id = {self.id_contrato};"
+        response = requests.post('http://82.208.22.228:8000/execute_query_sql_class/', json={"sql": sql_query})
+        print(response.json())
+    
     def checar_pessoa(self):
         if self.id_pessoa:
             try:
                 self.pessoa = Pessoas.objects.get(id=self.id_pessoa)
                 self.pessoa_existe = True
-                print(self.pessoa)
             except:
                 self.pessoa_existe = False
                 
     def adicionar_pessoa(self):
         if self.pessoa:
-            self.listas_pessoas = Pessoas.objects.filter(nome__icontains=self.pessoa.nome)
+            if self.pessoa.id not in self.listas_pessoas:
+                print('irei adicionar a pessoa')
+                #self.listas_pessoas.append(self.pessoa)
+                self.listas_pessoas.append({'nome': self.pessoa.nome,'id':self.pessoa.id, 'peso': self.peso})
+                #self.listas_pessoas[self.pessoa.id]['peso'] = self.peso
+                self.pessoa = None
+                self.id_pessoa = None
+                self.pessoa_existe = False
+                self.peso = 0
+            else:
+                print('pessoa ja existe no diciionario')
         
     
     def novo_contrato(self):
+        print(f"POST: {self.request.POST}")
         pass
