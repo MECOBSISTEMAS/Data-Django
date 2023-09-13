5e09686c3c223872ae1eb1b1dc0f143793a54033
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2023-09-13 14:40:33 -0300

    outra pagina para a listagem do cob diario pelas importações das planilhas

--- apps/home/existing_models.py
+++ apps/home/existing_models.py
@@ -345,6 +345,10 @@ class ContratoParcelas(models.Model):
     def rateio_calculado(self):
         return self.vl_parcela * (self.peso.valor/100)
     
+    @property
+    def repasse_calculado(self):
+        return self.rateio_calculado - (self.rateio_calculado * self.peso.pessoa.cliente.sim)
+    
     """ ao salvar a parcela preencher os campos de rateio e repasse com seus rescpectivos calculos """
 
     class Meta:
