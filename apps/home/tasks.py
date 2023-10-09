0bd32f943d778cee4bb4ab00be5cb8580c86b790
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2023-10-09 20:11:27 -0300

    configurações iniciais funcionando

--- apps/home/tasks.py
+++ apps/home/tasks.py
@@ -1,7 +1,27 @@
 from celery import shared_task
 from celery.schedules import crontab
+from core.celery import app
+from datetime import datetime
 
-
-@shared_task
+@app.task(name="criar_dados")
 def criar_dados():
-  return "FUNCIONANDO"
+    return "FUNCIONANDO"
+
+
+@app.task
+def escrever_em_arquivo():
+    now = datetime.now()
+    data_hora = now.strftime("%Y-%m-%d %H:%M:%S")
+    
+    with open("log.txt", "a") as arquivo:  # O modo "a" permite adicionar ao arquivo existente
+        arquivo.write(data_hora + "\n")
+
+
+
+""" @app.task(bind=True)
+def agendar_tarefa_criar_dados(sender, **kwargs):
+    sender.add_periodic_task(
+        crontab(minute="*"),
+        criar_dados.s(),
+        name="criar_dados",
+    ) """
--- core/celery.py
+++ core/celery.py
@@ -1,5 +1,6 @@
 import os
 
+from celery.schedules import crontab
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
@@ -18,7 +19,22 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
 
+""" app.conf.beat_schedule = {
+    'agendar-tarefa-criar-dados': {
+        'task': 'apps.home.tasks.agendar_tarefa_criar_dados',
+        'schedule': crontab(minute="*"),
+    },
+} """
+
+app.conf.beat_schedule = {
+    'agendar-escrever-em-arquivo': {
+        'task': 'apps.home.tasks.escrever_em_arquivo',
+        'schedule': crontab(minute="*"),
+    }
+}
+
 
 @app.task(bind=True, ignore_result=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
+    
